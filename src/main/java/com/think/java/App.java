ad4b3a21f5b9f8927c66c5dea0bc1953cee2dee5
Nguoi An Phu <nguoianphu@gmail.com>
2015-07-20 17:53:46 +0700

    remove special symbol

--- src/main/java/com/think/java/App.java
+++ src/main/java/com/think/java/App.java
@@ -89,8 +89,8 @@ public class App {
  * initialization.
  * 
  * Exercise 2: (1) Following the HelloDate.java example in this chapter, create
- * a “hello, world” program that simply displays that statement. You need only a
- * single method in your class (the “main” one that gets executed when the
+ * a hello, world program that simply displays that statement. You need only a
+ * single method in your class (the main one that gets executed when the
  * program starts). Remember to make it static and to include the argument list,
  * even though you don’t use the argument list. Compile the program with javac
  * and run it using java. If you are using a different development environment
