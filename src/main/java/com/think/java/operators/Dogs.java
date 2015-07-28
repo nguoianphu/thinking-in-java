17837f2a59d2c62070e1fbd481fabe3f0d956cb9
Nguoi An Phu <nguoianphu@gmail.com>
2015-07-28 18:46:33 +0700

    error: unmappable character for encoding UTF-8

--- src/main/java/com/think/java/operators/Dogs.java
+++ src/main/java/com/think/java/operators/Dogs.java
@@ -2,8 +2,8 @@ package com.think.java.operators;
 
 /**
  * Exercise 5: (2) Create a class called Dog containing two Strings: name and
- * says. In main( ), create two dog objects with names “spot” (who says,
- * “Ruff!”) and “scruffy” (who says, “Wurf!”). Then display their names and what
+ * says. In main( ), create two dog objects with names spot (who says,
+ * Ruff!) and scruffy (who says, Wurf!). Then display their names and what
  * they say.
  * 
  * @author vhtuan
